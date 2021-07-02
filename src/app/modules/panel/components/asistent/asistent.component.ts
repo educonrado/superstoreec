6c698fb0e7911cf3444c7c2396b7db4a6b38af01
Lina <excz.test@gmail.com>
2021-07-01 20:35:09 -0500

    Creación de asistente

--- src/app/modules/panel/components/asistent/asistent.component.ts
+++ src/app/modules/panel/components/asistent/asistent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-asistent',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsistentComponent implements OnInit {
 
-  constructor() { }
+  firstFormGroup: FormGroup = new FormGroup({});
+  secondFormGroup: FormGroup = new FormGroup({});
+  constructor(
+    private formBuilder: FormBuilder
+  ) { 
+    this.createForm();
+  }
+  private createForm(): void {
+    this.firstFormGroup = this.formBuilder.group({
+      nameStore: [null, [Validators.required]],
+      nameShortStore: [null, [Validators.required]],
+      descriptionStore: null,
+      imageStore: null,
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      instructionPay: null,
+      direction: null,
+      phoneStore: [null, [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
   }
