da2fd3eae69598c3be863b713762bb2a042f834e
edu_gcp <excz.test@gmail.com>
2021-11-12 22:49:11 +0000

    Mensaje de group prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'environments/environment';
+// Pipe
+import { TruncatePipe } from '@shared/util/truncate.pipe';
 
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent],
+  declarations: [AppComponent, LayoutComponent, TruncatePipe],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/util/truncate.pipe.ts
+++ src/app/shared/util/truncate.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, limite: string): string {
+    let limit = parseInt(limite);
+    return value.length > limit ? value.substring(0, limit) + "..." : value;
   }
 
 }
