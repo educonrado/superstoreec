871c4a259db1a87fb151a0dfd408ad99ede58b2b
Conrado Zapata <excz010715@sri.ad>
2021-12-05 08:03:47 -0500

    Igualar con master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,10 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'environments/environment';
-// Pipe
-import { TruncatePipe } from '@shared/util/truncate.pipe';
 
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, TruncatePipe],
+  declarations: [AppComponent, LayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
